6c55984cf3e031b794b2b1cdd3ce42938baf73cd
Chang Jung Lu <chang.jung.lu@example.com>
2025-07-28 16:25:43 +0800

    添加 ESLint 配置和建置測試腳本

---
+++ client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    'no-unused-vars': 'error',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  }
+}; 
